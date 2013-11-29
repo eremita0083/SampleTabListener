471bd7c8c3f38a1e2fcc6e92196c888032265c04
p-insider <p-insider@mail.goo.ne.jp>
2013-11-29 18:18:11 +0900

    HW

--- src/com/diyo/musiccreater0/MainActivity.java
+++ src/com/diyo/musiccreater0/MainActivity.java
@@ -1,79 +1,132 @@
 package com.diyo.musiccreater0;
 
-import java.util.Locale;
-
-import android.content.res.Configuration;
-import android.graphics.Typeface;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.GestureDetector;
-import android.widget.LinearLayout;
 
 public class MainActivity extends ActionBarActivity {
-	private MyTabListener myTabListener = null;
-	private ActionBarDrawerToggle mDrawerToggle;
-	private DrawerLayout mDrawer;
-	private String[] meibo = { "安達誠寛", "一瀬孝", "今井俊介", "岩崎大輔", "岩崎拓也", "岩塚美由紀",
-			"大司まり", "大津良馬", "嘉村翼", "楠元信吾", "桑原玲", "小西未央子", "佐藤章", "大力新太郎",
-			"高倉健治", "壇義弘", "能島章典", "野田幸代", "松島あゆみ", "松本真由美", "山口徹" };
-	private String[] jender = {"男", "男", "男", "男", "男", "女",
-			"女", "男", "男", "男", "男", "女", "男", "男",
-			"男", "男", "男", "女", "女", "女", "男"};
-	private MyCustomAdapter ad;
+	private Fragment m0Fragment, m1Fragment, m2Fragment, m3Fragment,
+			m4Fragment, m5Fragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// textview
-		Typeface typeface = Typeface
-				.createFromAsset(getAssets(), "regular.ttf");
+		setContentView(R.layout.main);
 
-		// drawer
-		mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer,
-				R.drawable.ic_drawer, R.string.drawer_open,
-				R.string.drawer_close) {
+		ActionBar ab = getActionBar();
+
+		ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS); // NavigationmodeTabsでタブがつく。
+
+		TabListener tabListener = new ActionBar.TabListener() {
+
+			@Override
+			public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			}
+
+			@Override
+			public void onTabSelected(Tab tab, FragmentTransaction ft) {
+				if (tab.getText().equals("受講者一覧")) {
+					//if (m0Fragment == null) {
+						m0Fragment = Tab0Fragment.instantiate(
+								getApplicationContext(),
+								Tab0Fragment.class.getName());
+						ft.add(android.R.id.content, m0Fragment, "受講者一覧");
+				//	} else {
+				//		ft.attach(m0Fragment);
+				//	}
+				} else if (tab.getText().equals("Group1")) {
+					if (m1Fragment == null) {
+						m1Fragment = Tab1Fragment.instantiate(
+								getApplicationContext(),
+								Tab1Fragment.class.getName());
+						ft.add(android.R.id.content, m1Fragment, "Group1");
+					} else {
+						ft.attach(m1Fragment);
+					}
+				} else if (tab.getText().equals("Group2")) {
+					if (m2Fragment == null) {
+						m2Fragment = Tab2Fragment.instantiate(
+								getApplicationContext(),
+								Tab2Fragment.class.getName());
+						ft.add(android.R.id.content, m2Fragment, "Group2");
+					} else {
+						ft.attach(m2Fragment);
+					}
+				} else if (tab.getText().equals("Group3")) {
+					if (m3Fragment == null) {
+						m3Fragment = Tab3Fragment.instantiate(
+								getApplicationContext(),
+								Tab3Fragment.class.getName());
+						ft.add(android.R.id.content, m3Fragment, "Group3");
+					} else {
+						ft.attach(m3Fragment);
+					}
+				} else if (tab.getText().equals("Group4")) {
+					if (m4Fragment == null) {
+						m4Fragment = Tab4Fragment.instantiate(
+								getApplicationContext(),
+								Tab4Fragment.class.getName());
+						ft.add(android.R.id.content, m4Fragment, "Group4");
+					} else {
+						ft.attach(m4Fragment);
+					}
+				} else if (tab.getText().equals("Group5")) {
+					if (m5Fragment == null) {
+						m5Fragment = Tab5Fragment.instantiate(
+								getApplicationContext(),
+								Tab5Fragment.class.getName());
+						ft.add(android.R.id.content, m5Fragment, "Group5");
+					} else {
+						ft.attach(m5Fragment);
+					}
+				}
+
+			}
+
+			//タブが選択されなくなった場合に、タブの情報をdetachする。
+			@Override
+			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+				if (tab.getText().equals("受講者一覧")) {
+					if (m0Fragment != null) {
+						ft.detach(m0Fragment);
+					}
+				} else if (tab.getText().equals("Group1")) {
+					if (m1Fragment != null) {
+						ft.detach(m1Fragment);
+					}
+				}else if (tab.getText().equals("Group2")) {
+					if (m2Fragment != null) {
+						ft.detach(m2Fragment);
+					}
+				}else if (tab.getText().equals("Group3")) {
+					if (m3Fragment != null) {
+						ft.detach(m3Fragment);
+					}
+				}else if (tab.getText().equals("Group4")) {
+					if (m4Fragment != null) {
+						ft.detach(m4Fragment);
+					}
+				}else if (tab.getText().equals("Group5")) {
+					if (m5Fragment != null) {
+						ft.detach(m5Fragment);
+					}
+				}
+			}
+
 		};
-		mDrawer.setDrawerListener(mDrawerToggle);
-		mDrawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-	
-		myTabListener = new MyTabListener();
-		// Tabをつける
-		ActionBar ab = getSupportActionBar();
-		ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS); //NavigationmodeTabsでタブがつく。Listでスピナがつく。
-		ab.addTab(ab.newTab().setText("受講者一覧").setTabListener(myTabListener)); //これでタブを追加.setTabListenerでタブを押した時の処理
-		ab.addTab(ab.newTab().setText("Group1").setTabListener(myTabListener)); //ActionBar.TabListenerを実装したクラスでそれを行う。
-		ab.addTab(ab.newTab().setText("Group2").setTabListener(myTabListener)); //基本はフラグメントを使う
-		ab.addTab(ab.newTab().setText("Group3").setTabListener(myTabListener)); //基本はフラグメントを使う
-		ab.addTab(ab.newTab().setText("Group4").setTabListener(myTabListener)); //基本はフラグメントを使う
-		ab.addTab(ab.newTab().setText("Group5").setTabListener(myTabListener)); //基本はフラグメントを使う
-		ab.setDisplayHomeAsUpEnabled(true); //これでiconをクリックするとホームに戻る。
-		ab.setHomeButtonEnabled(true); //こちらも一緒にsetする。
 
-		ad = new MyCustomAdapter(this, R.layout.my_listview_item, meibo);
+		// tabを追加
+		ab.addTab(ab.newTab().setText("受講者一覧").setTabListener(tabListener));
+		ab.addTab(ab.newTab().setText("Group1").setTabListener(tabListener));
+		ab.addTab(ab.newTab().setText("Group2").setTabListener(tabListener));
+		ab.addTab(ab.newTab().setText("Group3").setTabListener(tabListener));
+		ab.addTab(ab.newTab().setText("Group4").setTabListener(tabListener));
+		ab.addTab(ab.newTab().setText("Group5").setTabListener(tabListener));
 
-	}
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-		// DrawerToggleの状態を同期する
-		mDrawerToggle.syncState();
-	}
-
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-		// DrawerToggleの状態を同期する
-		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
 
 }
-
-
--- src/com/diyo/musiccreater0/MyCustomAdapter.java
+++ src/com/diyo/musiccreater0/MyCustomAdapter.java
@@ -1,18 +1,23 @@
 package com.diyo.musiccreater0;
 
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import android.content.Context;
 import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 
 class MyCustomAdapter extends ArrayAdapter<String> {
 	private int resourceId;
 	private Typeface typeface = Typeface.createFromAsset(getContext()
 			.getAssets(), "regular.ttf");
+	private Map<Integer,Boolean> checkCb = new LinkedHashMap<Integer, Boolean>();
 
 	public MyCustomAdapter(Context context, int resource, String[] objects) {
 		super(context, resource, objects);
@@ -22,18 +27,38 @@ class MyCustomAdapter extends ArrayAdapter<String> {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		convertView = View.inflate(getContext(), resourceId, null);
+		final int positionGv = position;
 
-		TextView text = (TextView) convertView
-				.findViewById(R.id.text_drawer_lv);
+		TextView text = (TextView) convertView.findViewById(R.id.tv);
+		final CheckBox cb = (CheckBox) convertView.findViewById(R.id.cb);
 
-		if (position == 0) {
-			text.setBackgroundResource(R.drawable.border);
+		cb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				if (isChecked) {
+					checkCb.put(positionGv, true);
+				} else if (!isChecked) {
+					checkCb.put(positionGv, false);
+				}
+
+			}
+		});
+		if(checkCb.get(positionGv)==null){
+			cb.setChecked(false);
+		}else{
+			cb.setChecked(checkCb.get(positionGv));
 		}
-
+		
 		text.setText(getItem(position).toString());
 		text.setTypeface(typeface);
 
 		return convertView;
+	}
+	
+	public  Map<Integer,Boolean> getCheckStatus() {
+		return checkCb;
 	}
 
 }
